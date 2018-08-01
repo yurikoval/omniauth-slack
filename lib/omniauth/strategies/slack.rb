213f4bfed991aaed47cac6023e51f7a971ca6cd8
wbr <wbr@mac.com>
2018-07-31 19:39:56 -0700

    Debug & cleanup workspace token support.

--- lib/omniauth/strategies/slack.rb
+++ lib/omniauth/strategies/slack.rb
@@ -75,6 +75,15 @@ module OmniAuth
         token_url: '/api/oauth.access'
       }
 
+      # Add team_domain to site subdomain if provided in auth url or strategy options. 
+      option :setup, lambda{|env|
+        strategy = env['omniauth.strategy']
+        team_domain = strategy.request.params['team_domain'] || strategy.options[:team_domain]
+        strategy.options[:client_options].site = (
+          !team_domain.to_s.empty? ? "https://#{team_domain}.slack.com" : "https://slack.com"
+        )
+      }
+      
       option :auth_token_params, {
         mode: :query,
         param_name: 'token'
@@ -85,37 +94,32 @@ module OmniAuth
       # User ID is not guaranteed to be globally unique across all Slack users.
       # The combination of user ID and team ID, on the other hand, is guaranteed
       # to be globally unique.
-      uid { "#{auth['user_id'] || auth['user'].to_h['id'] || auth['authorizing_user'].to_h['user_id']}-#{auth['team_id'] || auth['team'].to_h['id']}" }
+      uid { "#{user_id}-#{team_id}" }
+      
+      credentials do
+        {
+          #token: access_token.token,
+          token: auth['token'],
+          scope: (is_app_token ? all_scopes : auth['scope']),
+          expires: false
+        }
+      end
 
       info do
+        #puts "Requesting info"
         
-        # Experimental Thread Pool
-        if options.preload_data_with_threads.to_i > 0
-          work_q = Queue.new
-          %w(apps_permissions_users_list identity user_info user_profile team_info bot_info).each{|x| work_q.push x }
-          workers = (0...(options.preload_data_with_threads.to_i)).map do
-            Thread.new do
-              #puts "New thread #{Thread.current}"
-              begin
-                while x = work_q.pop(true)
-                  #puts "Processing '#{x}' with thread #{Thread.current}"
-                  send x
-                end
-              rescue ThreadError
-              end
-            end
-          end; "ok"
-          workers.map(&:join); "ok"
+        num_threads = options.preload_data_with_threads.to_i
+        if num_threads > 0 && !skip_info?
+          preload_data_with_threads(num_threads)
         end
-        
       
         # Start with only what we can glean from the authorization response.
         hash = { 
           name: auth['user'].to_h['name'],
           email: auth['user'].to_h['email'],
-          user_id: auth['user_id'] || auth['user'].to_h['id'] || auth['authorizing_user'].to_h['user_id'],
+          user_id: user_id,
           team_name: auth['team_name'] || auth['team'].to_h['name'],
-          team_id: auth['team_id'] || auth['team'].to_h['id'],
+          team_id: team_id,
           image: auth['team'].to_h['image_48']
         }
 
@@ -187,11 +191,12 @@ module OmniAuth
           #bot_info: bot_info,
           bot_info: auth['bot'] || bot_info['bot'],
           auth: auth,
-          identity: @identity,
-          user_info: @user_info,
-          user_profile: @user_profile,
-          team_info: @team_info,
-          scopes_requested: env['omniauth.strategy']&.options&.scope,
+          identity: identity,
+          user_info: user_info,
+          user_profile: user_profile,
+          team_info: team_info,
+          apps_permissions_users_list: apps_permissions_users_list,
+          scopes_requested: env['omniauth.strategy'] && env['omniauth.strategy'].options && env['omniauth.strategy'].options.scope,
           raw_info: {
             auth: access_token.dup.tap{|i| i.remove_instance_variable(:@client)},
             identity: @identity_raw,
@@ -201,14 +206,6 @@ module OmniAuth
             bot_info: @bot_info_raw,
             apps_permissions_users_list_raw: @apps_permissions_users_list_raw
           }
-        }
-      end
-      
-      credentials do
-        {
-          token: access_token.token,
-          scope: (is_app_token ? all_scopes : auth['scope']),
-          expires: false
         }
       end
       
@@ -232,14 +229,20 @@ module OmniAuth
       # Note that this behavior is entirely controlled by Slack. The omniauth-slack gem
       # only passes data to Slack and has no say in how the Slack OAUTH process works.
       #
-      def client
-        super.tap do |c|
-          team_domain = request.params['team_domain'] || authorize_params[:team_domain]
-          c.site = "https://#{team_domain}.slack.com" if !team_domain.to_s.empty?
-        end
-      end
+      # def client
+      #   puts "Requesting client"
+      #   super.tap do |c|
+      #     team_domain = request.params['team_domain'] || authorize_params[:team_domain]
+      #     #c.site = "https://#{team_domain}.slack.com" if !team_domain.to_s.empty?
+      #     c.site = "http://localhost:9292"
+      #   end
+      # end
       
+      
+      # TODO: Is there a better way to do this?
+      # This might be the best way... see https://github.com/omniauth/omniauth/issues/390
       def authorize_params
+        #puts "Requesting authorize_params"
         super.tap do |params|
           %w[scope team].each do |v|
             if !request.params[v].to_s.empty?
@@ -248,17 +251,49 @@ module OmniAuth
           end
         end
       end
+
+      # TODO: Find out why this is here and how it works.
+      # Ok... see here https://github.com/omniauth/omniauth/blob/master/lib/omniauth/strategy.rb
+      # This override cuts out the query_string... why?
+      # def callback_url
+      #   puts "Requesting callback_url"
+      #   puts "full_host: #{full_host}"
+      #   puts "script_name: #{script_name}"
+      #   puts "callback_path: #{callback_path}"
+      #   full_host + script_name + callback_path
+      # end
       
+      
+      private
+      
+      # Preload additional api calls with limited thread pool.
+      def preload_data_with_threads(num_threads=options.preload_data_with_threads.to_i)
+        #puts "Calling preload_data_with_threads"
+        log :info, "Calling preload_data_with_threads(#{num_threads})."
+        work_q = Queue.new
+        %w(apps_permissions_users_list identity user_info user_profile team_info bot_info).each{|x| work_q.push x }
+        workers = (0...(num_threads)).map do
+          Thread.new do
+            begin
+              while x = work_q.pop(true)
+                send x
+              end
+            rescue ThreadError
+            end
+          end
+        end; "ok"
+        workers.map(&:join); "ok"
+      end
+      
+      # Parsed data returned from /slack/oauth.access api call.
       def auth
-        @auth ||= access_token.params.to_h.merge({token: access_token.token})
+        @auth ||= access_token.params.to_h.merge({'token' => access_token.token})
       end
 
       def identity
-        return {} unless has_scope?('identity.basic') || has_scope?('identity:read:user')
-        @identity_raw ||= (
-          opts = {headers: {'X-Slack-User' => auth['user_id'] || auth['user']&.dig('id') || auth['authorizing_user']&.dig('user_id')}}
-          access_token.get( *['/api/users.identity', (is_app_token ? opts : nil)].compact )
-        )
+        #puts "Requesting identity"
+        return {} unless has_scope?(identity: ['identity.basic','identity:read:user'])
+        @identity_raw ||= access_token.get('/api/users.identity', headers: {'X-Slack-User' => user_id})
         @identity ||= @identity_raw.parsed
       end
 
@@ -271,71 +306,92 @@ module OmniAuth
       end
 
       def user_info
-        return {} unless has_scope?('users:read')
-        url = URI.parse('/api/users.info')
-        url.query = Rack::Utils.build_query(user: auth['user_id'] || auth['user'].to_h['id'])
-        url = url.to_s
-
-        @user_info_raw ||= access_token.get(url)
+        #puts "Requesting user_info"
+        return {} unless has_scope?(identity: 'users:read', team: 'users:read')
+        @user_info_raw ||= access_token.get('/api/users.info', params: {user: user_id}, headers: {'X-Slack-User' => user_id})
         @user_info ||= @user_info_raw.parsed
       end
       
       def user_profile
-        return {} unless has_scope?('users.profile:read')
-        url = URI.parse('/api/users.profile.get')
-        url.query = Rack::Utils.build_query(user: auth['user_id'] || auth['user'].to_h['id'])
-        url = url.to_s
-
-        @user_profile_raw ||= access_token.get(url)
+        #puts "Requesting user_profile"
+        return {} unless has_scope?(identity: 'users.profile:read', team: 'users.profile:read')
+        @user_profile_raw ||= access_token.get('/api/users.profile.get', params: {user: user_id}, headers: {'X-Slack-User' => user_id})
         @user_profile ||= @user_profile_raw.parsed
       end
 
       def team_info
-        return {} unless has_scope?('team:read')
+        #puts "Requesting team_info"
+        return {} unless has_scope?(identity: 'team:read', team: 'team:read')
         @team_info_raw ||= access_token.get('/api/team.info')
         @team_info ||= @team_info_raw.parsed
       end
 
       def web_hook_info
         return {} unless access_token.params.key? 'incoming_webhook'
-        access_token.params['incoming_webhook']
+        #access_token.params['incoming_webhook']
+        auth['incoming_webhook']
       end
       
       def bot_info
-        return {} unless has_scope?('users:read')
+        #puts "Requesting bot_info"
+        return {} unless has_scope?(identity: 'users:read')
         @bot_info_raw ||= access_token.get('/api/bots.info')
         @bot_info ||= @bot_info_raw.parsed
       end
       
-      
-      def apps_permissions_users_list
-        #return {} unless auth['token'].to_s[/^xoxa/]
-        return {} unless is_app_token
-        @apps_permissions_users_list_raw ||= access_token.get('/api/apps.permissions.users.list')
-        @apps_permissions_users_list ||= @apps_permissions_users_list_raw.parsed
+      def user_id
+        auth['user_id'] || auth['user'].to_h['id'] || auth['authorizing_user'].to_h['user_id']
       end
       
-      def has_scope?(scope)
-        #access_token['scope'].to_s.split(',').include?(scope.to_s)
-        #credentials&.dig(:scope).to_s.split(',').include?(scope.to_s)
-        all_scopes.each{|key,val| val.include?(scope.to_s)}
+      def team_id
+        auth['team_id'] || auth['team'].to_h['id']
+      end
+      
+      # Api all to get user permissions for workspace token.
+      # This is needed because workspace token 'sign-in-with-slack' is missing scopes
+      # in the :scope field.
+      #
+      # Returns [<id>: <resource>]
+      def apps_permissions_users_list
+        #puts "Requesting apps_permissions_users_list"
+        return {} unless is_app_token
+        @apps_permissions_users_list_raw ||= access_token.get('/api/apps.permissions.users.list')
+        @apps_permissions_users_list ||= @apps_permissions_users_list_raw.parsed['resources'].inject({}){|h,i| h[i['id']] = i; h}
       end
       
       def is_app_token
         auth['token_type'].to_s == 'app'
       end
       
+      # Scopes come from at least 3 different places now.
+      # * The classic :scope field (string)
+      # * New workshop token :scopes field (hash)
+      # * Separate call to apps.permissions.users.list (array)
+      #
+      # This returns hash of workspace scopes, with classic & new identity scopes in :identity.
+      # Lists of scopes are in array form.
       def all_scopes
         @all_scopes ||=
-        {'identity': auth['scope'] || apps_permissions_users_list&.dig('resources').to_a[0]&.dig('scopes').join(',')}
-        .merge(access_token['scopes'].to_h)
+        {'identity' => (auth['scope'] || apps_permissions_users_list[user_id].to_h['scopes'].join(',')).to_s.split(',')}
+        .merge(auth['scopes'].to_h)
+        #.merge(access_token['scopes'].to_h)
       end
-
-
-      private
-
-      def callback_url
-        full_host + script_name + callback_path
+      
+      # Determine if given scopes exist in current authorization.
+      # Scopes is hash where
+      #   key == scope type <identity|app_hope|team|channel|group|mpim|im>
+      #   val == array or string of individual scopes.
+      def has_scope?(**scopes_hash)
+        scopes_hash.detect do |section, scopes|
+          test_scopes = case
+            when scopes.is_a?(String); scopes.split(',')
+            when scopes.is_a?(Array); scopes
+            else raise "Scope must be a string or array"
+          end
+          test_scopes.detect do |scope|
+            all_scopes[section.to_s].to_a.include?(scope.to_s)
+          end
+        end
       end
       
     end
